3a1a252f14f3c4002535fb9534e6057abc152e59
Andrey <andreyforprog@gmail.com>
2019-12-27 09:38:08 +0300

    index dont ended

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,6 +3,7 @@ const headerSection = document.querySelector('.header_section')
 const headerSubProd = document.querySelector('.header_subproduct')
 const headerProd = document.querySelectorAll('.header_product-main')
 const headerTest = document.querySelector('.header_test')
+const headerSectionProduct = document.querySelector('.header_section-product')
 console.log(headerTest)
 
 for (let i = 0; i<headerLinks.length; i++) {
@@ -13,3 +14,24 @@ for (let i = 0; i<headerLinks.length; i++) {
     })
   })
 }
+
+for (let i = 0; i<headerProd.length; i++) {
+  headerProd[i].addEventListener('mouseover', () => {
+    headerSubProd.style.display = 'block'
+    headerSectionProduct.addEventListener('mouseleave', () => {
+      headerSubProd.style.display = 'none'
+    })
+  })
+}
+
+// for mobile
+const burgerBtn = document.querySelector('#menu__toggle')
+const burgerMenu = document.querySelector('.header-mobile_link')
+
+burgerBtn.addEventListener('click', () => {
+  if (burgerMenu.style.display === 'flex') {
+    burgerMenu.style.display = 'none'
+  } else {
+    burgerMenu.style.display = 'flex'
+  }
+})
