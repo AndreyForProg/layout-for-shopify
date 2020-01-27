9f25d738b6e488c7f3eca3b9c6fe690784874cf6
Andrey <andreyforprog@gmail.com>
2020-01-28 01:33:05 +0300

    to prod

--- assets/js/product.js
+++ assets/js/product.js
@@ -26,6 +26,7 @@ const reviewsTabs = document.querySelector('#reviewsTabs');
 
 
 reviewsTabs.addEventListener('click', () => {
+  console.log('mob')
   reviewsTitle.classList.remove('product-tabs_title')
   reviewsTabs.classList.add('product-tabs_title')
   reviewsTitle.classList.add('product-tabs')
@@ -40,7 +41,46 @@ reviewsTitle.addEventListener('click', () => {
   reviewsTabs.classList.add('product-tabs')
   reviewsFild.style.display = "flex";
   reviewsAsk.style.display = "none";
-
 });
 
+// mobile tabs
+const reviewsTabsM = document.querySelector('#reviewsTabs_mob');
+const reviewsTitleM = document.querySelector('#reviewsTitle_mob');
+
+reviewsTabsM.addEventListener('click', (e) => {
+  activMobTab(e)
+  reviewsTitleM.classList.remove('active_mob_tab')
+  reviewsAsk.style.display = "flex";
+  reviewsFild.style.display = "none";
+})
+reviewsTitleM.addEventListener('click', (e) => {
+  activMobTab(e)
+  reviewsTabsM.classList.remove('active_mob_tab')
+  reviewsFild.style.display = "flex";
+  reviewsAsk.style.display = "none";
+})
+
+function activMobTab(e) {
+  e.target.classList.toggle('active_mob_tab')
+}
+// утв mobile tabs
+
+
 $('.easyzoom').easyZoom();
+
+// products slider
+$('.slider_content.desctop').slick({
+  arrows: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4
+});
+
+$('.slider_content.mobile').slick({
+  arrows: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 2
+});
